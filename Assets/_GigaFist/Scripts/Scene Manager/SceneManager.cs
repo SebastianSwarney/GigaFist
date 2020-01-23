59fc35aa4433c7a28006f328b1eafe7bdc660fa5
Alexandre Bujold <50187131+AlexandreBujold@users.noreply.github.com>
2020-01-23 14:18:21 -0500

    Added Scene Fading and Indexes
    - Added Fade coroutine to SceneManager.cs
    - Tested above functionality in SceneManager scene
    - Created SceneIndexes.cs, a public enum meant to held build index values for scenes for easy reference

--- Assets/_GigaFist/Scripts/Scene Manager/SceneManager.cs
+++ Assets/_GigaFist/Scripts/Scene Manager/SceneManager.cs
@@ -2,26 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace GigaFist
 {
     public class SceneManager : MonoBehaviour
     {
+        public static SceneManager instance;
+        public CanvasGroup canvasGroup;
+        public float fadeTime;
+        public AnimationCurve fadeCurve = new AnimationCurve();
+
         // Start is called before the first frame update
         void Start()
         {
-            
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+
         }
 
-        public void ChangeScene(string sceneName)
+        public void ChangeScene(SceneIndexes scene)
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+            UnityEngine.SceneManagement.SceneManager.LoadScene((int)scene, LoadSceneMode.Additive);
+        }
+
+        private IEnumerator Fade(bool fadeOut, float transitionTime)
+        {            
+            //Assign starting alpha based off of desired fade
+            float alpha = 0;
+
+            for (float t = 0; t <= 1; t += 1 / (transitionTime / Time.deltaTime))
+            {
+                //Calculate Alpha value
+                if (fadeOut)
+                {
+                    alpha = fadeCurve.Evaluate(t);
+                }
+                else
+                {
+                    alpha = 1 - fadeCurve.Evaluate(t);
+                }
+                
+                //Apply alpha value to canvas group
+                if (canvasGroup != null)
+                {
+                    canvasGroup.alpha = alpha;
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
+            yield return new WaitForEndOfFrame();
         }
     }
 
