345c26babede861f225e93061780e3cc5225a5b4
Alexandre Bujold <alexandre.bujold@outlook.com>
2020-01-27 20:15:07 -0500

    Updated SceneManager.cs
    - Added SetLoadingScreen() to handle setting visibility of the loading screen, and control if it animates or not
    - Added inspector organization for SceneManager.cs
    - Added LoadScene to simply load scenes
    - Added ChangeScene to handle changing scenes

--- Assets/_GigaFist/Scripts/Scene Manager/SceneManager.cs
+++ Assets/_GigaFist/Scripts/Scene Manager/SceneManager.cs
@@ -8,13 +8,22 @@ namespace GigaFist
 {
     public class SceneManager : MonoBehaviour
     {
+        [Header("Scene Management Properties")]
         public static SceneManager instance;
-        public CanvasGroup canvasGroup;
+        public bool unloadCurrentOnChange = true;
+        public SceneIndexes currentScene = SceneIndexes.SAMPLE_SCENE;
+
+        [Header("Loading Screen Properties")]
+        public CanvasGroup loadingScreen;
+        public bool animate = true;
         public float fadeTime;
         public AnimationCurve fadeCurve = new AnimationCurve();
+        private bool loadingScreenVisible = false;
+        private Coroutine transitionAnimation;
 
         [HideInInspector]
         public List<AsyncOperation> loadingScenes;
+        public List<Scene> loadedScenes;
 
         // Start is called before the first frame update
         void Start()
@@ -29,36 +38,96 @@ namespace GigaFist
             }
         }
 
+        //LoadScene should simply LOAD the scene
+        //ChangeScenes should LOAD if not loaded, and then change to that scene (set as active scene)
+
         // Update is called once per frame
         void Update()
         {
-
+            if (Input.GetMouseButtonDown(0))
+            {
+                SetLoadingScreen(!loadingScreenVisible, animate);
+            }
         }
 
-        public void ChangeScene(SceneIndexes scene)
+        public void LoadScene(SceneIndexes scene)
         {
             loadingScenes.Add(UnityEngine.SceneManagement.SceneManager.LoadSceneAsync((int)scene, LoadSceneMode.Additive));
         }
 
-        private IEnumerator Fade(bool fadeOut, float transitionTime)
-        {            
+        public void ChangeScene(SceneIndexes scene)
+        {
+            if (unloadCurrentOnChange)
+            {
+                loadingScenes.Add(UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync((int)currentScene));
+            }
+
+            LoadScene(scene);
+
+        }
+
+        public void SetLoadingScreen(bool visible, bool animate)
+        {
+            if (visible)
+            {
+                if (animate)
+                {
+                    if (transitionAnimation == null)
+                    {
+                        transitionAnimation = StartCoroutine(Fade(true, fadeTime));
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    loadingScreen.alpha = 1;
+                }
+                loadingScreenVisible = true;
+            }
+            else
+            {
+                if (animate)
+                {
+                    if (transitionAnimation == null)
+                    {
+                        transitionAnimation = StartCoroutine(Fade(false, fadeTime));
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    loadingScreen.alpha = 0;
+                }
+                loadingScreenVisible = false;
+            }
+        }
+
+        private IEnumerator Fade(bool fadeIn, float transitionTime)
+        {
             //Assign starting alpha based off of desired fade
             float alpha = 0;
 
             for (float t = 0; t <= 1; t += 1 / (transitionTime / Time.deltaTime))
             {
                 //Calculate Alpha value
-                alpha = fadeOut == true ? alpha = fadeCurve.Evaluate(t): alpha = 1 - fadeCurve.Evaluate(t);
-                
+                alpha = fadeIn == true ? alpha = fadeCurve.Evaluate(t) : alpha = 1 - fadeCurve.Evaluate(t);
+
                 //Apply alpha value to canvas group
-                if (canvasGroup != null)
+                if (loadingScreen != null)
                 {
-                    canvasGroup.alpha = alpha;
+                    loadingScreen.alpha = alpha;
                 }
 
                 yield return new WaitForEndOfFrame();
             }
             yield return new WaitForEndOfFrame();
+            transitionAnimation = null;
         }
     }
 
