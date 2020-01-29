71554c88d5e9affa22fc0dee4b486ceb346ebf16
RollTheGatorade <49420411+RollTheGatorade@users.noreply.github.com>
2020-01-29 00:09:42 -0500

    FallZone
    Created a box collider to check if player has fallen off of the platform as a prefab
    created a script to attach to player to check if fallen
    created a test scene

---
+++ Assets/_GigaFist/Scripts/FallZoneCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallZoneCollision : MonoBehaviour
+{
+    public Transform resetZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        print(hit);
+        if (hit.gameObject.tag == "FallZone")
+        {
+            print("fall");
+           transform.position = resetZone.position;
+        }
+    }
+
+}
