9019cddd8acec220e2f54ce9e11190b90c77aa51
RollTheGatorade <49420411+RollTheGatorade@users.noreply.github.com>
2020-01-30 19:56:15 -0500

    ColourSelectImplemented
    Finished basic implementation of characterSelect script, materials switch when player 1 uses arrow keys

--- Assets/_GigaFist/Scripts/CharacerSelect.cs
+++ Assets/_GigaFist/Scripts/CharacerSelect.cs
@@ -6,17 +6,45 @@ public class CharacerSelect : MonoBehaviour
 {
 
     //change colours
+    public Material[] playerColours;
+    public Material currentColour;
+    public int currentlySelected;
+    MeshRenderer characterMesh;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterMesh = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        currentColour = playerColours[currentlySelected];
+        characterMesh.material = currentColour;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //reset if going up after reached end of colour options
+            if (currentlySelected == playerColours.Length-1)
+            {
+                currentlySelected = 0;
+            }
+            else { currentlySelected++; }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentlySelected == 0)
+            {
+                currentlySelected = playerColours.Length-1;
+            }
+            else { currentlySelected--; }
+
+        }
     }
+
+
 }
