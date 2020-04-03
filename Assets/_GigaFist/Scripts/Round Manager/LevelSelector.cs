a904c1f002955920cebc188853a4f38d53d13f95
Alexandre Bujold <c.alexandre.bujold@outlook.com>
2020-04-03 13:39:35 -0400

    Add Auto Select Level 2

--- Assets/_GigaFist/Scripts/Round Manager/LevelSelector.cs
+++ Assets/_GigaFist/Scripts/Round Manager/LevelSelector.cs
@@ -6,18 +6,28 @@ using GigaFist;
 public class LevelSelector : MonoBehaviour //Just a simple script to load the selected level and call MatchManager
 {
     public bool autoSelectLevel1 = false;
+    public bool autoSelectLevel2 = false;
 
     private void Start()
     {
         if (autoSelectLevel1)
         {
-            Invoke("SelectLevel1", 1.5f);
+            Invoke("SelectLevel1", 1f);
+        }
+        else if (autoSelectLevel2)
+        {
+            Invoke("SelectLevel2", 1f);
         }
     }
 
     private void SelectLevel1()
     {
         MatchManager.Instance.SetSelectLevel((int)SceneIndexes.CHAR_TEST);
+    }
+
+    private void SelectLevel2()
+    {
+        MatchManager.Instance.SetSelectLevel((int)SceneIndexes.LEVEL_ONE);
     }
 
     public void SelectLevel(int levelIndex)
