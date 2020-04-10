c6063624e11593d7a7349b84a0b6003327038cf1
Alexandre Bujold <c.alexandre.bujold@outlook.com>
2020-04-09 20:36:49 -0400

    Add Trail & Particle Effect to Combat
    - Add Trail
    - Add Particle Effects for Hitting and being damaged

---
+++ Assets/_GigaFist/Scripts/Player/PlayerEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEffect : MonoBehaviour
+{
+
+    public GameObject onHitEffectPrefab;
+    public GameObject onDamageEffectPrefab;
+
+
+    public void SpawnHitEffect(int id)
+    {
+        if (onHitEffectPrefab != null)
+        {
+            Instantiate(onHitEffectPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void SpawnDamageEffect(int id)
+    {
+        if (onDamageEffectPrefab != null)
+        {
+            Instantiate(onDamageEffectPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
