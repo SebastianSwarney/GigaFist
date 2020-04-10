b4fb8271e367503992d07cc7bdd172937e7ffc7c
RollTheGatorade <49420411+RollTheGatorade@users.noreply.github.com>
2020-04-09 20:16:42 -0400

    punchbuildup and UI scaling
    added ui scaling for main menu and loading screen
    added robots on main menu THEY ARE NOW STUCK HANGING OUT IN THE GAME LEVEL HELP
    also added a punch build up script

---
+++ Assets/_GigaFist/Scripts/Player/PlayerUIBuildUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIBuildUp : MonoBehaviour
+{
+    public PlayerController playerScript;
+    public Image fistBuildupImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fistBuildupImage.fillAmount= playerScript.m_punchChargePercent;
+    }
+}
